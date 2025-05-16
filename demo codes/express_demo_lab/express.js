48113e69369ad70b0f074d2cd297f57c859ac457
Fourthhy <miguelmanabo4@gmail.com>
2025-05-16 21:49:34 +0800

    Save Changes before Creating another branch

--- demo codes/express_demo_lab/express.js
+++ demo codes/express_demo_lab/express.js
@@ -4,6 +4,9 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json())
+app.use(express.encodeurl())
+
 app.get("/", (req, res) => {
     res.send("Hi, Universe");
 });
